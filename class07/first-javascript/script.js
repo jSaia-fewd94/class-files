bdbe744ef6a8afc5d00f675a6f802eee1bcb16ca
jksaia14 <jksaia14@gmail.com>
2016-09-01 21:39:00 -0400

    java script exercise

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,43 @@
+var num1 = 10;
+document.write(num1);
+console.log(num1);
+
+document.write('<br>');
+
+var num2 = 10;
+document.write(num2);
+console.log(num2);
+
+document.write('<br>');
+
+var num3 = 33;
+document.write(num3);
+console.log(num3);
+
+document.write('<br>');
+
+var num4 = 67;
+document.write(num4);
+console.log(num4);
+
+document.write('<br>');
+
+document.write('Jill Saia');
+
+
+
+
+
+
+
+
+//function
+function doSomething(){
+
+	console.log('this is working');
+}
+
+// call the function
+doSomething ();
+
+
