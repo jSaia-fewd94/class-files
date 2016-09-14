40564cbe1518307543bcec855878b09651c3bbf7
jksaia14 <jksaia14@gmail.com>
2016-09-14 10:27:49 -0400

    class exercises

---
+++ class10/class-work10/jquery-city/js/app.js
@@ -0,0 +1,13 @@
+// 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
+// 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
+
+$('#first').click(function().attr('src', 'ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
+
+$('#second').click(function().attr('src', 'ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
+
+$('#third').click(function().attr('src', 'ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
+
+$('#fourth').click(function().attr('src', 'ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
+
+$('#bigimage').click(function().attr('src', 'ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js');
+
---
+++ class10/class-work10/jquery-review/js/script.js
@@ -0,0 +1,10 @@
+$(".box").css("background-color", "blue");
+
+
+$("#one").click(function(){
+	$("#one").css("background-size", "500px");
+})
+
+$("#two").click(function(){
+	$("#two").css("background-size", "500px");
+})
