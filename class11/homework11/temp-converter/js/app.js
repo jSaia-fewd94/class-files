e5eef9ac6bf791bc3e0ade7ea1fe375422669459
jksaia14 <jksaia14@gmail.com>
2016-09-25 18:12:19 -0400

    class 13

--- class11/homework11/temp-converter/js/app.js
+++ class11/homework11/temp-converter/js/app.js
@@ -1,9 +1,20 @@
-$('#submit').click(function(){
-  alert(parseInt($('#temp').val()) - 32 / 1.8);
-  if (temp > 70){
-    alert('It is hot!');
-  }
-  else if (temp < 70){
-    alert('It is cold!');
-  }
+$(document).ready(function(){
+
+	var rawTemp;
+	var convTemp;
+	var rawUnit;
+	var convUnit; 
+
+	$('#submit').click(function(){
+		rawTemp = $('#input').val();
+		rawUnit = $('select').val();
+		if(rawUnit === 'f'){
+			convTemp = (rawTemp - 32) / 1.8;
+			convUnit = "C";
+		}else{
+			convTemp = (rawTemp * 1.8) + 32;
+			convUnit = "F";
+		}
+		$('#output').html(convTemp + " " + convUnit);
+	});
 });
